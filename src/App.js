56260221d8231b21edb8e5832111f3bffeb72019
mattia richetto <mattia.richetto@gmail.com>
2020-04-27 13:43:28 -0700

    add form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
+const CHANGE_BIO = 'CHANGE_BIO';
+const CHANGE_NAME = 'CHANGE_NAME';
+const CHANGE_URL = 'CHANGE_URL';
+
 function App() {
-  return <div className="App">TODO</div>;
+  function reducer(state, action) {
+    console.log(action);
+    switch (action.type) {
+      case CHANGE_BIO:
+        return { ...state, bio: action.value };
+      case CHANGE_NAME:
+        return { ...state, name: action.value };
+      case CHANGE_URL:
+        return { ...state, url: action.value };
+      default:
+        return state;
+    }
+  }
+
+  const initializerArgs = { bio: '', name: '', url: '' };
+
+  const [{ bio }, dispatch] = useReducer(reducer, initializerArgs);
+
+  return (
+    <div className="App">
+      <h1 className="App__h1">Public profile</h1>
+      <form className="App__form">
+        <input
+          className="App__input"
+          name="name"
+          onChange={(e) =>
+            dispatch({ type: CHANGE_NAME, value: e.target.value })
+          }
+          placeholder="Name"
+          type="text"
+        />
+        <textarea
+          className="App__textarea"
+          cols="40"
+          name="bio"
+          onChange={(e) =>
+            dispatch({ type: CHANGE_BIO, value: e.target.value })
+          }
+          placeholder="Bio"
+          rows="10"
+          value={bio}
+        />
+        <input
+          className="App__input"
+          name="url"
+          onChange={(e) =>
+            dispatch({ type: CHANGE_URL, value: e.target.value })
+          }
+          placeholder="URL"
+          type="text"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default App;
