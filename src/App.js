febb986ccd5537f644238e140aa7cf50181f257c
mattia richetto <mattia.richetto@gmail.com>
2020-04-30 16:51:05 -0700

    use blacklist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useReducerWithLocalStorage from 'use-reducer-with-local-storage';
-import reducer, { emptyState } from './store/reducer';
+import reducer, {
+  emptyState,
+  LOADING,
+  READY,
+  SET_STATUS
+} from './store/reducer';
 import './App.css';
 import makeHandleOnChange from './makeHandleOnChange';
 
 function App() {
-  const [{ bio, name, url }, dispatch] = useReducerWithLocalStorage({
+  const [{ bio, name, status, url }, dispatch] = useReducerWithLocalStorage({
+    blacklist: ['status'],
     initializerArg: emptyState,
     key: 'REACT_APP_STATE',
     reducer
@@ -13,44 +19,58 @@ function App() {
 
   const handleOnChange = makeHandleOnChange({ dispatch });
 
+  useEffect(() => {
+    if (status === LOADING) {
+      setTimeout(() => {
+        dispatch({ type: SET_STATUS, value: READY });
+      }, 3000);
+    }
+  }, [dispatch, status]);
+
   return (
     <div className="App">
-      <h1 className="App__h1">Public profile</h1>
-      <form className="App__form">
-        <input
-          className="App__input"
-          name="name"
-          onChange={handleOnChange}
-          placeholder="Name"
-          type="text"
-          value={name}
-        />
-        <textarea
-          className="App__textarea"
-          cols="40"
-          name="bio"
-          onChange={handleOnChange}
-          placeholder="Bio"
-          rows="10"
-          value={bio}
-        />
-        <input
-          className="App__input"
-          name="url"
-          onChange={handleOnChange}
-          placeholder="URL"
-          type="text"
-          value={url}
-        />
-        <button
-          className="App__button"
-          name="clear"
-          onClick={handleOnChange}
-          type="button"
-        >
-          Clear
-        </button>
-      </form>
+      {status === LOADING ? (
+        <h1 className="App__h1">Loading ...</h1>
+      ) : (
+        <>
+          <h1 className="App__h1">Public profile</h1>
+          <form className="App__form">
+            <input
+              className="App__input"
+              name="name"
+              onChange={handleOnChange}
+              placeholder="Name"
+              type="text"
+              value={name}
+            />
+            <textarea
+              className="App__textarea"
+              cols="40"
+              name="bio"
+              onChange={handleOnChange}
+              placeholder="Bio"
+              rows="10"
+              value={bio}
+            />
+            <input
+              className="App__input"
+              name="url"
+              onChange={handleOnChange}
+              placeholder="URL"
+              type="text"
+              value={url}
+            />
+            <button
+              className="App__button"
+              name="clear"
+              onClick={handleOnChange}
+              type="button"
+            >
+              Clear
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
