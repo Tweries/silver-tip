6941bc67ea79d546e5370babcf2aeb520df39cff
mattia richetto <mattia.richetto@gmail.com>
2020-05-01 18:41:39 -0700

    Updates

---
+++ precache-manifest.158c68595a8a68ef3f3ba873b7c161c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b885e1aa40339271de4c1ce01f7cfd19",
+    "url": "/silver-tip/index.html"
+  },
+  {
+    "revision": "38430b3dfbef360c83e2",
+    "url": "/silver-tip/static/css/main.cd064127.chunk.css"
+  },
+  {
+    "revision": "949828ec1dc67d70f838",
+    "url": "/silver-tip/static/js/2.511acf47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/silver-tip/static/js/2.511acf47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38430b3dfbef360c83e2",
+    "url": "/silver-tip/static/js/main.3ed97772.chunk.js"
+  },
+  {
+    "revision": "b43350717fcab6593978",
+    "url": "/silver-tip/static/js/runtime-main.c8ea3bf9.js"
+  }
+]);
